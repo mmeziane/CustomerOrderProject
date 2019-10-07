80501af33a9f509c4b12de84c4e0e48317962a3c
mmeziane <mmeziane@hotmail.nl>
2019-10-08 00:44:26 +0200

    @PreAuthorize annotation added and the methods findByOrderId and findByOrderName added.
    Signed-off-by: mmeziane <mmeziane@hotmail.nl>

--- CustomerOrderProject/src/main/java/com/mmeziane/demo/repository/CustomerRepository.java
+++ CustomerOrderProject/src/main/java/com/mmeziane/demo/repository/CustomerRepository.java
@@ -1,12 +1,20 @@
 package com.mmeziane.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import com.mmeziane.demo.model.Customer;
 
 
+@PreAuthorize("hasRole('ROLE_USER')")
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+	
+	public List<Customer> findByOrderId(long orderId);
+	
+	public List<Customer> findByOrderName(String orderName);
 
 }
