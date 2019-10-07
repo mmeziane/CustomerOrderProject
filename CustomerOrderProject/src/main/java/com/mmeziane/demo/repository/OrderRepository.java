15d594489acda69f543a0c8f4f5dfa8351be2424
mmeziane <mmeziane@hotmail.nl>
2019-10-08 00:59:59 +0200

    Added comment (Only users who have a role ROLE_USER can invoke it ).
    Signed-off-by: mmeziane <mmeziane@hotmail.nl>

--- CustomerOrderProject/src/main/java/com/mmeziane/demo/repository/OrderRepository.java
+++ CustomerOrderProject/src/main/java/com/mmeziane/demo/repository/OrderRepository.java
@@ -6,6 +6,9 @@ import org.springframework.stereotype.Repository;
 
 import com.mmeziane.demo.model.Order;
 
+/*
+ * Only users who have a role ROLE_USER can invoke it
+ */
 @PreAuthorize("hasRole('ROLE_USER')")
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
