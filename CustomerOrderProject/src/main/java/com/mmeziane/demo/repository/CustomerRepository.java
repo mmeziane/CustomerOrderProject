633b1b35b0eb6c294798f01d651ff0aa430e98bb
mmeziane <mmeziane@hotmail.nl>
2019-10-08 00:13:25 +0200

    JPARepository extended and @Repository annotation added.
    Signed-off-by: mmeziane <mmeziane@hotmail.nl>

--- CustomerOrderProject/src/main/java/com/mmeziane/demo/repository/CustomerRepository.java
+++ CustomerOrderProject/src/main/java/com/mmeziane/demo/repository/CustomerRepository.java
@@ -1,5 +1,12 @@
 package com.mmeziane.demo.repository;
 
-public interface CustomerRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mmeziane.demo.model.Customer;
+
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long>{
 
 }
--- CustomerOrderProject/src/main/java/com/mmeziane/demo/repository/OrderRepository.java
+++ CustomerOrderProject/src/main/java/com/mmeziane/demo/repository/OrderRepository.java
@@ -1,5 +1,11 @@
 package com.mmeziane.demo.repository;
 
-public interface OrderRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mmeziane.demo.model.Order;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long>{
 
 }
