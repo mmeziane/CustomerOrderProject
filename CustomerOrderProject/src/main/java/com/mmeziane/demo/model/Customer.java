0b341775958d82b196685ca6ee5653414998f9fd
mmeziane <mmeziane@hotmail.nl>
2019-10-07 23:43:49 +0200

    setters and getters added Added constructors added hashcode() and equals() and toString() objects

--- CustomerOrderProject/src/main/java/com/mmeziane/demo/model/Customer.java
+++ CustomerOrderProject/src/main/java/com/mmeziane/demo/model/Customer.java
@@ -1,5 +1,132 @@
 package com.mmeziane.demo.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
 public class Customer {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String street;
+	
+	@OneToMany
+	private Order order;
+
+	public Customer() {
+		
+	}
+
+	public Customer(String firstName, String lastName, String street, Order order) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.street = street;
+		this.order = order;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public Order getOrder() {
+		return order;
+	}
+
+	public void setOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((order == null) ? 0 : order.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (id != other.id)
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (order == null) {
+			if (other.order != null)
+				return false;
+		} else if (!order.equals(other.order))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", street=" + street
+				+ ", order=" + order + "]";
+	}
+	
+	
+	
+	
+	
+	
 
 }
--- CustomerOrderProject/src/main/java/com/mmeziane/demo/model/Order.java
+++ CustomerOrderProject/src/main/java/com/mmeziane/demo/model/Order.java
@@ -1,5 +1,112 @@
 package com.mmeziane.demo.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
 public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
+	
+	private String orderName;
+	
+	private String details;
+	
+	@ManyToOne
+	private Customer customer;
+	
+	public Order() {
+		
+	}
+
+	public Order(String orderName, String details, Customer customer) {
+		super();
+		this.orderName = orderName;
+		this.details = details;
+		this.customer = customer;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getOrderName() {
+		return orderName;
+	}
+
+	public void setOrderName(String orderName) {
+		this.orderName = orderName;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	public Customer getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(Customer customer) {
+		this.customer = customer;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((customer == null) ? 0 : customer.hashCode());
+		result = prime * result + ((details == null) ? 0 : details.hashCode());
+		result = prime * result + (int) (id ^ (id >>> 32));
+		result = prime * result + ((orderName == null) ? 0 : orderName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		if (customer == null) {
+			if (other.customer != null)
+				return false;
+		} else if (!customer.equals(other.customer))
+			return false;
+		if (details == null) {
+			if (other.details != null)
+				return false;
+		} else if (!details.equals(other.details))
+			return false;
+		if (id != other.id)
+			return false;
+		if (orderName == null) {
+			if (other.orderName != null)
+				return false;
+		} else if (!orderName.equals(other.orderName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Order [id=" + id + ", orderName=" + orderName + ", details=" + details + ", customer=" + customer + "]";
+	}
+	
+	
+	
 
 }
