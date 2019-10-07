2d03c9c29d2942d155a4979131c84b184c1be09d
mmeziane <mmeziane@hotmail.nl>
2019-10-07 23:47:23 +0200

    @Entity annotation added

--- CustomerOrderProject/src/main/java/com/mmeziane/demo/model/Customer.java
+++ CustomerOrderProject/src/main/java/com/mmeziane/demo/model/Customer.java
@@ -1,10 +1,12 @@
 package com.mmeziane.demo.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
+@Entity
 public class Customer {
 	
 	@Id
