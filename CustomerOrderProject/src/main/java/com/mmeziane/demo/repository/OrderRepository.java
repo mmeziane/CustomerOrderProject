c4124c5f97c5fae6097e992182fb3e67652db2be
mmeziane <mmeziane@hotmail.nl>
2019-10-08 00:56:42 +0200

    Added annotation @PreAuthorize("hasRole('ROLE_USER')").
    Signed-off-by: mmeziane <mmeziane@hotmail.nl>

--- CustomerOrderProject/src/main/java/com/mmeziane/demo/repository/OrderRepository.java
+++ CustomerOrderProject/src/main/java/com/mmeziane/demo/repository/OrderRepository.java
@@ -1,10 +1,12 @@
 package com.mmeziane.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import com.mmeziane.demo.model.Order;
 
+@PreAuthorize("hasRole('ROLE_USER')")
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
 
