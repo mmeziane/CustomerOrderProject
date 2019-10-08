8c1d456983c80f31a780ccbf4162a11c8b7883b2
mmeziane <mmeziane@hotmail.nl>
2019-10-08 13:29:31 +0200

    Remove unused imports
    Signed-off-by: mmeziane <mmeziane@hotmail.nl>

--- CustomerOrderProject/src/main/java/com/mmeziane/demo/controller/OrderController.java
+++ CustomerOrderProject/src/main/java/com/mmeziane/demo/controller/OrderController.java
@@ -1,8 +1,6 @@
 package com.mmeziane.demo.controller;
 
 import java.util.List;
-import java.util.Optional;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
